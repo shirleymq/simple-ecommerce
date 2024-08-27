aac5827c81ae1d384951df5dc4e79a614e82a25c
Shirley Quelali <shirley.quelali@gmail.com>
2024-08-27 19:39:10 -0400

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
+  darkMode: "class", // o 'media' para usar la preferencia del sistema
+
   theme: {
     extend: {},
   },
